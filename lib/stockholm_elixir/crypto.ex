8af888790771602a359d6a60847ac22d6a2bc39b
Karl Eklund <localpart@gmail.com>
2015-02-03 01:16:00 +0100

    Remove deprecation warning

--- lib/stockholm_elixir/crypto.ex
+++ lib/stockholm_elixir/crypto.ex
@@ -1,6 +1,6 @@
 defmodule StockholmElixir.Crypto do
   def md5(string) do
-    :crypto.md5(string)
+    :crypto.hash(:md5, string)
     |> :erlang.bitstring_to_list
     |> Enum.map(fn(x) -> :erlang.integer_to_binary(x, 16) end)
     |> :erlang.list_to_binary
