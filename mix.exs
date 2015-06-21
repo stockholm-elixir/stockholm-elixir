fce9dd59f93b365c9353e267a6e5aecc8776d641
Henrik Nyh <henrik@nyh.se>
2015-06-21 10:26:22 +0200

    Remove unused chat and oauth code

--- mix.exs
+++ mix.exs
@@ -15,16 +15,20 @@ defmodule StockholmElixir.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [mod: {StockholmElixir, []},
-     applications: [:phoenix, :cowboy, :logger, :httpoison]]
+     applications: [
+       :phoenix,
+       :cowboy,
+       :logger,
+     ]]
   end
 
   # Specifies your project dependencies
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 0.8.0"},
-     {:cowboy, "~> 1.0"},
-     {:oauth2, "~> 0.0.3"},
-     {:httpoison, "0.5.0"}]
+    [
+      {:phoenix, "~> 0.8.0"},
+      {:cowboy, "~> 1.0"},
+    ]
   end
 end
